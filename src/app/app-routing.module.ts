4e66c85b52007b007d637ededad414940015ab12
raul55lp <49528540+raul55lp@users.noreply.github.com>
2020-04-07 10:06:00 +0200

    Base pre firebase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import { PostComponent } from './components/posts/post/post.component';
 const routes: Routes = [
   { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) },
   { path: 'post/:id', component: PostComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'about', loadChildren: () => import('./components/pages/about/about.module').then(m => m.AboutModule) }
   // {path: 'posts', loadChildren: () => import('./components/posts/list-posts/list-posts.module').then(m => m.ListPostsModule)}
 ];
 
